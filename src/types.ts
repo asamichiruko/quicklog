50272c0984c76d09e5de624a68425c8517d6d309
Asami Chiruko <asami.chiruko@gmail.com>
2026-05-31 20:10:00 +0900

    chore: SyncOperation および QuicklogData type を追加

--- src/types.ts
+++ src/types.ts
@@ -9,3 +9,19 @@ export type AppSettings = {
 }
 
 export type ExportType = "json" | "markdown"
+
+export type SyncOperationType = "delete"
+
+export type SyncOperation =
+  | {
+    id: string
+    type: "delete"
+    entryId: string
+    createdAt: string
+  }
+
+export type QuicklogData = {
+  version: number
+  logEntries: LogEntry[]
+  syncOperations: SyncOperation[]
+}
